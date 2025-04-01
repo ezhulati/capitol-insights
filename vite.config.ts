44bc661bdc182a20537e26d3af9513b9158c358b
ezhulati <ezhulati@users.noreply.github.com>
2025-04-01 17:27:04 -0500

    FIX: Remove unused env variable in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 import viteCompression from 'vite-plugin-compression';
@@ -8,8 +8,7 @@ import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
 export default defineConfig(({ mode }) => {
-  // Load env variables
-  const env = loadEnv(mode, process.cwd());
+  // Analyze bundle in analyze mode
   const analyzeBundle = mode === 'analyze';
 
   return {
